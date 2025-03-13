9466ab43b9538792cdcad34af2f48177b7d6530a
Max <max94220@hotmail.fr>
2025-03-13 16:55:21 +0100

    correction<user>

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -48,8 +48,11 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
-            'token' => $token, 
-            'user' => $user]);
+            'token' => $token,
+            'name' => $user->name,
+            'email' => $user->email,
+            'roles' => $user->getRoleNames(),
+        ]);
 
     }
 
